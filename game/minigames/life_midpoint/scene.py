1e2d58839580239a484541b3f148ca0f0c6d7e12
IIIADRIENIII <136937257+IIIADRIENIII@users.noreply.github.com>
2025-09-18 10:07:13 +0200

    Fixing tolerance to 0 in midlife game

--- game/minigames/life_midpoint/scene.py
+++ game/minigames/life_midpoint/scene.py
@@ -7,7 +7,7 @@ from ...utils import blit_text_center, clamp, draw_attempts, load_sound, render_
 
 class LifeMidpointScene(Scene):
     CURSOR_SPEED = LIFE_KEY_SPEED  # pixels per second for keyboard
-    TOLERANCE_YEARS = 1  # years within target for success
+    TOLERANCE_YEARS = 0  # years within target for success
 
     def __init__(self, game):
         super().__init__(game)
